ba4087a510f031c11c88b026529249ee44e7eba3
Cameron Goss <camerongoss10@gmail.com>
2020-03-10 09:14:57 +0200

    initial commit

---
+++ ECommerce.Api.Products/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Models
+{
+    //This model is designed to return data from the provider
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Inventory { get; set; }
+    }
+}
---
+++ ECommerce.Api.Products/Providers/ProductsProvider.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using ECommerce.Api.Products.Database;
+using ECommerce.Api.Products.Interfaces;
+using ECommerce.Api.Products.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Providers
+{
+    public class ProductsProvider : IProductsProvider
+    {
+        private readonly ProductsDbContext dbContext;
+        private readonly ILogger<ProductsProvider> logger;
+        private readonly IMapper mapper;
+
+        public ProductsProvider(ProductsDbContext dbContext, ILogger<ProductsProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            if (!dbContext.Products.Any())
+            {
+                for (var i = 0; i < 5; i++)
+                { 
+                    dbContext.Products.Add(new Database.Product() { Id = i+1, Name = "Product " + i.ToString(), Price = i, Inventory = i });
+                }
+
+                //dbContext.Products.Add(new Database.Product() { Id = 1, Name = "Keyboard", Price = 75, Inventory = 25 });
+                //dbContext.Products.Add(new Database.Product() { Id = 2, Name = "Mouse", Price = 40, Inventory = 60 });
+                //dbContext.Products.Add(new Database.Product() { Id = 3, Name = "Monitor", Price = 150, Inventory = 10 });
+                //dbContext.Products.Add(new Database.Product() { Id = 4, Name = "Mousepad", Price = 20, Inventory = 30 });
+                //dbContext.Products.Add(new Database.Product() { Id = 5, Name = "Router", Price = 60, Inventory = 15 });
+
+                dbContext.SaveChanges();
+                
+            }
+        }
+
+        public async Task<(bool isSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync()
+        {
+            try
+            {
+                var products = await dbContext.Products.ToListAsync();
+                if(products != null && products.Any())
+                {
+                    var result = mapper.Map<IEnumerable<Database.Product>, IEnumerable<Models.Product>>(products);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch(Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
